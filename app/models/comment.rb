87fb88a473accb51ff1dd3f438184153fa460749
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 22:10:19 +0430

    create update_comment_counter method for comment table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, :class_name =>'User', foreign_key: :author_id
   belongs_to :post
+  after_save :update_comment_counter
+
+  def update_comment_counter
+    Post.find_by_id(self.post_id).increment!(:comments_counter)
+  end  
 end    

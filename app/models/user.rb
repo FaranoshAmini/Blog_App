3ead566cf45dcd298870e33bdf96b352288b234d
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 22:09:09 +0430

    Create recent_posts method for user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-
+  
+  def recent_posts
+    Post.where(author: self).order(updated_at: :desc).limit(3)
+  end
 end

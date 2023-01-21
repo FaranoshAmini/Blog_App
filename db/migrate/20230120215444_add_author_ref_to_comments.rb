c6d48872397f2a2ff80b20315d8642970aba9e64
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 13:24:48 +0430

    Fix Linter errors

--- db/migrate/20230120215444_add_author_ref_to_comments.rb
+++ db/migrate/20230120215444_add_author_ref_to_comments.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddAuthorRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :author, null: false, foreign_key: {to_table: :users}
+    add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
   end
 end

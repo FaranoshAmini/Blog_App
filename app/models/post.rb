99cadb87ff24df56d7bd5d7b2c5e791dc4ae0b35
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-22 01:13:57 +0430

    Add unit spec for user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :post_counter_update
 
-  validates :Title, presence: true, length: { maximum: 250 }
-  validates :PostsCounter, numericality: { greater_than_or_equal_to: 0 }
-  validates :LikesCounter, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
   
   def post_counter_update
     User.find_by_id(author_id).increment!(:posts_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
  
-  validates :Name, presence: true
-  validates :Posts_Counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
     Post.where(author: self).order(updated_at: :desc).limit(3)

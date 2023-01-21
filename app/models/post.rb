45606f5230ead3ebf139d0e1d03225dbc0194495
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 23:45:33 +0430

    Add validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :post_counter_update
 
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :PostsCounter, numericality: { greater_than_or_equal_to: 0 }
+  validates :LikesCounter, numericality: { greater_than_or_equal_to: 0 }
+  
   def post_counter_update
     User.find_by_id(author_id).increment!(:posts_counter)
   end

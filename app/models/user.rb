d7825860f278b3013879e45b38de50bf4f871b8d
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 23:45:16 +0430

    Add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+ 
+  validates :Name, presence: true
+  validates :Posts_Counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
     Post.where(author: self).order(updated_at: :desc).limit(3)

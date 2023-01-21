624e16759a2872124de36e3c2f7ee2f0f7c5b6c3
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-21 22:09:55 +0430

    Create post_counter_update and recent_comments method for post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, :class_name =>'User', foreign_key: :author_id
   has_many :likes
   has_many :comments
+  after_save :post_counter_update
+
+  def post_counter_update
+    User.find_by_id(self.author_id).increment!(:posts_counter)
+  end 
+  
+  def recent_comments
+    Comment.where(post: self).order(updated_at: :desc).limit(5)
+  end
 end

cec59a2dd77a889a8ce3fd9f1a85c51b82f8f109
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-28 22:53:53 +0430

    Implement the required changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   def create
     @new_like = Like.new(post_id: params[:post_id], author_id: current_user.id)
-    redirect_to users_path if @new_like.save
+    redirect_to users_path if @new_like.save else redirect_to :new
   end
 end

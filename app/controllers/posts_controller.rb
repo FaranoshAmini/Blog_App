5e0700afb3089055c46cc3ca3708024dc1554d28
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-28 05:10:48 +0430

    Add methods for the index and show pages of the posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author: @user)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post: @post)
+  end
 end

d6b6e87b09289a65b662c9cad2f754e6ed13b33c
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-28 21:47:31 +0430

    Add a route for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+  resources :posts, only: [:new, :create] do 
+    resources :comments, only: [:new, :create]
+  end
+  resources :likes, only: [:create]
 end

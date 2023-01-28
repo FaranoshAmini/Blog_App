6c80814fa397dfabdef79707cd215083fc0c1001
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-01-28 23:11:08 +0430

    Fix the rubocop error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,6 @@ class CommentsController < ApplicationController
 
   def create
     @new_comment = Comment.new(text: params[:comment][:text], author: current_user, post_id: params[:post_id].to_i)
-    redirect_to users_path if @new_comment.save else redirect_to :new
+    redirect_to users_path if @new_comment.save else redirect_to :new end
   end
 end

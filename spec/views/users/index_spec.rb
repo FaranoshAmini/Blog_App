446f01f6c61d375d007bdc9ae71aa42ce941c93b
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2023-02-03 01:05:35 +0430

    Add test to check usernames in index_spec.rb

---
+++ spec/views/users/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Users index view', type: :system do
+  describe 'Index page' do
+    before(:each) do
+      @first_user = User.create(name: 'Tom', photo: 'https://img.freepik.com/free-vector/floral-card_53876-91231.jpg', bio: 'my bio')
+      @second_user = User.create(name: 'Farah', photo: 'https://img.freepik.com/free-vector/floral-card_53876-91231.jpg', bio: 'Doctor')
+      @users = User.all
+    end
+    it  'Display  username of all user\'s' do
+      @users.each do |user|
+      expect(page).to have_content(user.name)
+      end 
+    
+      
+    end
+  end
+end
